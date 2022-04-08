b42bb9454576f0e730a0930f2969ab93e35b61d7
Thanh pham <thanhpham.23102000@gmail.com>
2022-04-08 17:25:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <input v-model="newTask" />
+    <button @click="addTask" class="btn">Add task</button>
+    <TaskComp
+      v-for="(task, index) in tasks"
+      :key="index"
+      :taskData="task"
+      :id="index"
+      @clicked="deleteTask"
+    />
+  </div>
+</template>
+<script>
+import TaskComp from './components/TaskComp';
+export default {
+  data() {
+    return {
+      newTask: '',
+      tasks: [],
+    };
+  },
+  methods: {
+    addTask: function () {
+      this.tasks.push({
+        content: this.newTask,
+        done: false,
+      });
+    },
+    deleteTask: function (id) {
+      console.log('id', id);
+      this.tasks.splice(id, 1);
+    },
+  },
+  // watch: {
+  //   newTask: function (newValue, oldValue) {
+  //     console.log('old: ' + oldValue);
+  //     console.log('new: ' + newValue);
+  //     console.log('this.newTask', this.newTask);
+  //   },
+  // },
+  components: {
+    TaskComp,
+  },
+};
+</script>
+
+<style>
+.btn {
+  background-color: #fff;
+  color: #333;
+  border-radius: 20px;
+  border: 1px solid #333;
+  width: 80px;
+}
+</style>
---
+++ src/components/TaskComp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="task-comp">
+    <label class="label">
+      <input type="checkbox" :checked="taskData.done" @click="onChecked" />
+      <span v-bind:class="{ done: taskData.done }">
+        {{ taskData.content }}
+      </span>
+    </label>
+    <button class="btn" @click="handleDelete">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TaskComp',
+  props: ['taskData', 'clicked', 'id'],
+  methods: {
+    onChecked() {
+      this.$props.taskData.done = !this.$props.taskData.done;
+    },
+    handleDelete() {
+      console.log('this.props', this.$props.id);
+      this.$emit('clicked', this.$props.id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.label {
+  margin-right: 8px;
+}
+.btn {
+  cursor: pointer;
+}
+.done {
+  text-decoration: line-through;
+}
+</style>
